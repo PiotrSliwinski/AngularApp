d56ebb00532de669f40d52ad0ef4fdafc5f238ed
Sliwinski <pcs@ANYACCESS.NET>
2017-06-14 17:54:24 +0100

    progress
    Signed-off-by: Sliwinski <pcs@ANYACCESS.NET>

--- app/Events/event-thumbnail.component.ts
+++ app/Events/event-thumbnail.component.ts
@@ -25,9 +25,7 @@ import { Component, Input } from '@angular/core'
             </div>         
     </div>
     `,
-    styles: [`
-    .green {color: #003300 !important; }
-    .bold { font-weight: bold; }
+    styles: [`    
     .thumbnail { min-height: 210px; }
     .pad-left {margin-left: 10px;}
     .well div {color: #bbb;}
